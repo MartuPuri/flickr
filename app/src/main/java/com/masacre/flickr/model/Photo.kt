74e1b01d2a32ebfb50f490ff1bd092ce61f6fb88
Martin Purita <martinpurita@konquer.app>
2020-10-03 12:47:57 -0700

    Add complete photo model

--- app/src/main/java/com/masacre/flickr/model/Photo.kt
+++ app/src/main/java/com/masacre/flickr/model/Photo.kt
@@ -1,8 +1,27 @@
 package com.masacre.flickr.model
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.annotation.JsonNaming
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class Photo(
-    var id: String? = null
+    var id : String? = null,
+    var farm : Int? = null,
+    @JsonProperty("height_s")
+    var heightS : String? = null,
+    @JsonProperty("isfamily")
+    var isFamily : Int? = null,
+    @JsonProperty("isfriend")
+    var isFriend : Int? = null,
+    @JsonProperty("ispublic")
+    var isPublic : Int? = null,
+    var owner : String? = null,
+    var secret : String? = null,
+    var server : String? = null,
+    var title : String? = null,
+    @JsonProperty("url_s")
+    var url : String? = null,
+    @JsonProperty("width_s")
+    var widthS : String? = null
 )
